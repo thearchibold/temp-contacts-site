81df2e3a6e1d29bf352861ddae1a756ee8f30ce8
Archibold Bernard <aephisarh@gmail.com>
2026-04-16 06:33:50 +0000

    facebook integration

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,11 @@
 import type { Metadata } from 'next'
 import { Geist, Geist_Mono } from 'next/font/google'
 import { Analytics } from '@vercel/analytics/next'
+import Script from 'next/script'
+
+// @ts-ignore
 import './globals.css'
+
 
 const geist = Geist({
   subsets: ["latin"],
@@ -102,7 +106,35 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={`${geist.variable} ${geistMono.variable}`}>
+      <head>
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: 'none' }}
+            src="https://www.facebook.com/tr?id=1406957651189163&ev=PageView&noscript=1"
+          />
+        </noscript>
+      </head>
       <body className="font-sans antialiased">
+        <Script
+          id="meta-pixel"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '1406957651189163');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
         {children}
         <Analytics />
       </body>
